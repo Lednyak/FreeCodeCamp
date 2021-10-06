3d2fed7000664420b63425106cc773b27f93f7f4
Lednyak <lednyak@tut.by>
2021-10-06 21:41:42 +0300

    JS

---
+++ script.js
@@ -0,0 +1,4 @@
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";   /*Escaping Literal Quotes in StringsPassed*/
+
+var myStr = "This is the start. " + "This is the end.";  /*Concatenating Strings with Plus Operator*/ 
+console.log(myStr); 
