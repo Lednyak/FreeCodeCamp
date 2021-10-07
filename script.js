f0232fd886673909b635d6d612994536db2bb441
Lednyak <lednyak@tut.by>
2021-10-07 23:43:08 +0300

    JS

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ var myStr = "I am a \"double quoted\" string inside \"double quotes\".";   /*Esc
 
 var myStr = "This is the start. " + "This is the end.";  /*Concatenating Strings with Plus Operator*/ 
 console.log(myStr); 
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; /*Escape Sequences in Strings*/
+
+var myStr = "This is the first sentence. "  /*Concatenating Strings with the Plus Equals Operator*/
+myStr += "This is the second sentence."
+console.log(myStr);
